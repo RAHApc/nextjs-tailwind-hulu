e8ab833695aaf697ba838ff467ddc2218d342e0b
RAHApc <pcaro2398@gmail.com>
2022-08-23 10:41:55 +0430

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
+  // const rewrites = () => {
+  //   return [{
+  //     source: "/cats",
+  //     destination: "https://meowfacts.herokuapp.com",
+  //   }, ];
+  // };
+  // return {
+  //   rewrites,
+  // };
   reactStrictMode: true,
+  images: {
+    domains: ['links.papareact.com', 'image.tmdb.org']
+  },
 }
